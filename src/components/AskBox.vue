1b9ab8573c2531499d7a01611c4e74f18754ba45
sevilaygndgn <65922544+sevilaygndgn@users.noreply.github.com>
2022-07-28 22:02:11 +0300

    Added: ask box, home categories, question article

---
+++ src/components/AskBox.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="ask-box">
+    <div class="avatar">
+      <img
+        class="avatar-photo"
+        src="https://secure.gravatar.com/avatar/?s=96&d=mm&r=g"
+        alt=""
+      />
+    </div>
+    <div class="box-question">
+      <div class="message-square">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 40 40"
+          fill="none"
+          stroke="#677075"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-message-square"
+        >
+          <path
+            d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"
+          ></path>
+        </svg>
+      </div>
+      <span class="box-text">What's your question?</span>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+  color: #000;
+}
+ul {
+  list-style-type: none;
+}
+.ask-box {
+  padding: 30px;
+  display: flex;
+  background-color: $main-background;
+  border-bottom: solid 2px #e4e6e6;
+  .avatar {
+    margin-right: 30px;
+    &-photo {
+      width: 40px;
+      height: 40px;
+      border-radius: 100%;
+      overflow: hidden;
+    }
+  }
+  .box-question {
+    border: 1px solid #e4e6e6;
+    height: 40px;
+    padding: 10px;
+    font-size: 12px;
+    font-weight: 400;
+    width: calc(100% - 50px);
+    display: flex;
+    .message-square {
+      height: 15px;
+      width: 15px;
+      margin-right: 10px;
+    }
+    .box-text {
+      color: #677075;
+      font-family: $arial;
+      &:hover {
+        color: $blue;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/HomeCategories.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="categories">
+    <ul class="categories-menu">
+      <li>
+        <a class="active" href="#">Recent Questions</a>
+      </li>
+      <li>
+        <a href="#">Most Answered</a>
+      </li>
+      <li>
+        <a href="#">Bump Questions</a>
+      </li>
+      <li>
+        <a href="#">Answers</a>
+      </li>
+      <li>
+        <a href="#">Most Visited</a>
+      </li>
+      <li>
+        <a href="#">Most Voted</a>
+      </li>
+      <li>
+        <a href="#">No Answers</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+  color: #000;
+}
+ul {
+  list-style-type: none;
+}
+.categories {
+  height: 80px;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+  padding-right: 30px;
+  background-color: $main-background;
+  border-bottom: solid 2px #e4e6e6;
+  .categories-menu {
+    height: 100%;
+    display: flex;
+    list-style-type: none;
+    overflow: auto;
+    li {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      margin-right: 20px;
+      a {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        color: $text-gray-100;
+        white-space: nowrap;
+        transition: ease-in-out 0.3s;
+        &:hover {
+          color: $primary-color;
+          border-bottom: 5px solid;
+        }
+        &.active {
+          color: $primary-color;
+          border-bottom: 5px solid;
+        }
+      }
+    }
+  }
+}
+</style>
