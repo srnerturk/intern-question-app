2fc40195549c0fa69c2b232e5b5e4191dfe3379b
Serhat Yurdakul <serhatyurdakulsl@gmail.com>
2022-07-29 11:45:51 +0300

    Cart stats added
    Cart stats added

--- src/components/CardStats.vue
+++ src/components/CardStats.vue
@@ -1,7 +1,49 @@
 <template>
   <div class="stats-container">
     <div class="statistics">
-      <slot />
+      <div class="home">
+        <h2>
+          Stats
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-bar-chart-2"
+          >
+            <line x1="18" y1="20" x2="18" y2="10"></line>
+            <line x1="12" y1="20" x2="12" y2="4"></line>
+            <line x1="6" y1="20" x2="6" y2="14"></line>
+          </svg>
+        </h2>
+        <div class="statistics-grid">
+          <div class="card square">
+            <p class="title">Questions</p>
+            <p class="count">22</p>
+          </div>
+
+          <div class="card square">
+            <p class="title">Answers</p>
+            <p class="count">72</p>
+          </div>
+
+          <div class="card square">
+            <p class="title">Best Answers</p>
+            <p class="count">11</p>
+          </div>
+
+          <div class="card square">
+            <p class="title">Users</p>
+            <p class="count">101</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
