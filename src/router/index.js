f254073003961d4fd373a6d3f215765c7262fed7
Hakkı Can Akut <32385870+HakkiAkut@users.noreply.github.com>
2022-08-01 17:34:34 +0300

    views :: question view created

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView";
+import QuestionView from "../views/QuestionView";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "HomeView",
     component: HomeView,
+  },
+  {
+    path: "/question",
+    name: "QuestionView",
+    component: QuestionView,
   },
 ];
 
