c314f89483b50b294dd90ee14054d8ca6217f235
sevilaygndgn <65922544+sevilaygndgn@users.noreply.github.com>
2022-08-01 15:25:52 +0300

    Added home-view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <master-view>
+      <div class="column main">
+        <ask-box />
+        <home-categories />
+        <question-articles />
+      </div>
+    </master-view>
+  </div>
+</template>
+<script>
+import MasterView from "@/components/MasterView.vue";
+import AskBox from "@/components/AskBox.vue";
+import HomeCategories from "@/components/HomeCategories.vue";
+import QuestionArticles from "@/components/QuestionArticles.vue";
+export default {
+  components: { MasterView, AskBox, HomeCategories, QuestionArticles },
+  name: "HomeView",
+};
+</script>
+<style lang="scss">
+.main {
+  max-width: 820px;
+}
+</style>
